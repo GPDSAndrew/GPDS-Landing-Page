c78961728e3e4425bea501921c6b79498ffc7aca
GPDSAndrew <Andrew@GPDS.com>
2025-04-03 21:29:15 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 });
             },
             enter(data) {
+                window.scrollTo(0, 0);  // Force scroll to the top when loading new page
                 return gsap.from(data.next.container, {
                     opacity: 0,
                     duration: 0.5
