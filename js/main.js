f507e7226caf7b543a0dea1476123b12ef8458a0
GPDSAndrew <Andrew@GPDS.com>
2025-04-03 22:30:06 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,21 @@ document.addEventListener("DOMContentLoaded", function() {
                 return gsap.to(data.current.container, { opacity: 0, duration: 0.6 });
             },
             enter(data) {
-                window.scrollTo(0, 0); // Ensure we start at the top of the new page
+                window.scrollTo(0, 0);
                 return gsap.from(data.next.container, { opacity: 0, duration: 0.6 });
             }
         }],
         views: [{
             namespace: 'index',
+            beforeEnter(data) {
+                document.body.classList.add('index-background');
+            },
+            beforeLeave(data) {
+                document.body.classList.remove('index-background');
+            }
+        },
+        {
+            namespace: 'about',
             beforeEnter(data) {
                 document.body.classList.add('index-background');
             },
