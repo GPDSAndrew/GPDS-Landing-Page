65bf0cc12ac104b6b610ba1a7d5826f832fe4a98
GPDSAndrew <Andrew@GPDS.com>
2025-04-03 22:08:37 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     barba.init({
         transitions: [{
-            
             name: 'fade',
             leave(data) {
                 return gsap.to(data.current.container, { opacity: 0, duration: 0.6 });
             },
             enter(data) {
-                window.scrollTo(0, 0);
-                  return gsap.from(data.next.container, { opacity: 0, duration: 0.6 });
+                window.scrollTo(0, 0); // Ensure we start at the top of the new page
+                return gsap.from(data.next.container, { opacity: 0, duration: 0.6 });
+            }
+        }],
+        views: [{
+            namespace: 'index',
+            beforeEnter(data) {
+                document.body.classList.add('index-background');
+            },
+            beforeLeave(data) {
+                document.body.classList.remove('index-background');
             }
         }]
     });
