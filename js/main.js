8762df6d1ab3db2185a65c0d627296789dd6da93
GPDSAndrew <Andrew@GPDS.com>
2025-04-03 22:46:47 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 return gsap.to(data.current.container, { opacity: 0, duration: 0.6 });
             },
             enter(data) {
-                window.scrollTo(0, 0); // This line is good for starting fresh, but we need more.
+                window.scrollTo(0, 0); 
+                document.body.style.overflow = 'auto';  // Allow scrolling again
                 return gsap.from(data.next.container, { opacity: 0, duration: 0.6 });
             }
         }],
@@ -15,20 +16,22 @@ document.addEventListener("DOMContentLoaded", function() {
                 namespace: 'index',
                 beforeEnter(data) {
                     document.body.classList.add('index-background');
-                    document.body.style.overflow = 'auto'; // Allow scrolling
+                    document.body.style.overflow = 'auto'; // Allow scrolling on the index page
                 },
                 beforeLeave(data) {
                     document.body.classList.remove('index-background');
+                    document.body.style.overflow = 'hidden'; // Prevent scrolling during transition
                 }
             },
             {
                 namespace: 'about',
                 beforeEnter(data) {
                     document.body.classList.add('index-background');
-                    document.body.style.overflow = 'auto'; // Allow scrolling
+                    document.body.style.overflow = 'auto'; // Allow scrolling on the about page
                 },
                 beforeLeave(data) {
                     document.body.classList.remove('index-background');
+                    document.body.style.overflow = 'hidden'; // Prevent scrolling during transition
                 }
             }
         ]
